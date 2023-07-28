8319a40655e09ca426db83d29b06871c36b44183
Mohsen Afshari <mohsenafshari75@yahoo.com>
2023-07-29 00:19:23 +0330

    Changed styles of elements

--- src/Projects/CentralOffice.tsx
+++ src/Projects/CentralOffice.tsx
@@ -4,34 +4,38 @@ export const CentralOffice = () => {
   return (
     <>
       <h2>Development of Processes for Central Office of ACECR</h2>
-      <p>
-        The goal of this project was for the center office of ACECR to be able
-        to help manage other branches.
-      </p>
-      <p>
-        My role in this project initially was to develop the remaining parts of
-        processes, but later it transformed into cleaning up the code and
-        unifying the common functions.
-      </p>
-      <p>
-        Technologies used in this project: JQuery (for forms), vue js and Kendo
-        UI (for viewing grid data), php and laravel (for defining REST APIs),
-        MySQL.
-      </p>
+      <section>
+        <p>
+          The goal of this project was for the center office of ACECR to be able
+          to help manage other branches.
+        </p>
+        <p>
+          My role in this project initially was to develop the remaining parts
+          of processes, but later it transformed into cleaning up the code and
+          unifying the common functions.
+        </p>
+        <p>
+          Technologies used in this project: JQuery (for forms), vue js and
+          Kendo UI (for viewing grid data), php and laravel (for defining REST
+          APIs), MySQL.
+        </p>
+      </section>
       <PersianH2>توسعه سامانه جامع جهاد دفتر مرکزی</PersianH2>
-      <PersianParagraph>
-        هدف این پروژه این بود که دفتر مرکزی جهاد دانشگاهی را در مدیریت شعبه‌های
-        دیگرش کمک کند.
-      </PersianParagraph>
-      <PersianParagraph>
-        نقش من در این پروژه در ابتدا برای توسعه بخش‌های باقی‌مانده از فرآیندها
-        بود، اما بعدها تبدیل به تمیزکردن کد و یکپارچه‌سازی توابع مشترک شد.
-      </PersianParagraph>
-      <PersianParagraph>
-        تکنولوژی‌های استفاده شده در این پروژه عبارتند از: JQuery (برای فرم‌ها)،
-        Vue.js و Kendo UI (برای مشاهده داده‌ها به صورت گرید اطلاعات)، PHP و
-        Laravel (برای تعریف رابط‌های برنامه‌نویسی REST) و MySQL
-      </PersianParagraph>
+      <section>
+        <PersianParagraph>
+          هدف این پروژه این بود که دفتر مرکزی جهاد دانشگاهی را در مدیریت
+          شعبه‌های دیگرش کمک کند.
+        </PersianParagraph>
+        <PersianParagraph>
+          نقش من در این پروژه در ابتدا برای توسعه بخش‌های باقی‌مانده از فرآیندها
+          بود، اما بعدها تبدیل به تمیزکردن کد و یکپارچه‌سازی توابع مشترک شد.
+        </PersianParagraph>
+        <PersianParagraph>
+          تکنولوژی‌های استفاده شده در این پروژه عبارتند از: JQuery (برای
+          فرم‌ها)، Vue.js و Kendo UI (برای مشاهده داده‌ها به صورت گرید اطلاعات)،
+          PHP و Laravel (برای تعریف رابط‌های برنامه‌نویسی REST) و MySQL
+        </PersianParagraph>
+      </section>
     </>
   );
 };
--- src/Projects/Projects.tsx
+++ src/Projects/Projects.tsx
@@ -1,4 +1,6 @@
+import styled from "styled-components";
 import { PersianH1 } from "../CommonStyles";
+import { useTheme } from "../Providers/ThemeProvider";
 import { CentralOffice } from "./CentralOffice";
 import { MiniLibrary } from "./MiniLibrary";
 import { MovieSearchApp } from "./MovieSearchApp";
@@ -7,16 +9,57 @@ import { SampleExperimentM } from "./SampleExperimentM";
 import { WeatherApp } from "./WeatherApp";
 
 export const Projects = () => {
+  const styles = useTheme();
+
+  const ModifiedProjectWrapper = styled(ProjectWrapper).attrs(() => ({
+    style: {
+      backgroundColor: styles.surfaceColor,
+      color: styles.color,
+    },
+  }))``;
+
+  const ModifiedWrapper = styled(Wrapper).attrs(() => ({
+    style: {
+      backgroundColor: styles.backgroundColor,
+      color: styles.color,
+    },
+  }))``;
+
   return (
-    <>
+    <ModifiedWrapper>
       <h1>Projects</h1>
       <PersianH1>پروژه‌ها</PersianH1>
-      <MiniLibrary />
-      <SampleExperimentCD />
-      <SampleExperimentM />
-      <CentralOffice />
-      <WeatherApp />
-      <MovieSearchApp />
-    </>
+      <ModifiedProjectWrapper>
+        <MiniLibrary />
+      </ModifiedProjectWrapper>
+      <ModifiedProjectWrapper>
+        <SampleExperimentCD />
+      </ModifiedProjectWrapper>
+      <ModifiedProjectWrapper>
+        <SampleExperimentM />
+      </ModifiedProjectWrapper>
+      <ModifiedProjectWrapper>
+        <CentralOffice />
+      </ModifiedProjectWrapper>
+      <ModifiedProjectWrapper>
+        <WeatherApp />
+      </ModifiedProjectWrapper>
+      <ModifiedProjectWrapper>
+        <MovieSearchApp />
+      </ModifiedProjectWrapper>
+    </ModifiedWrapper>
   );
 };
+
+const ProjectWrapper = styled.article`
+  padding: 1rem;
+`;
+
+const Wrapper = styled.section`
+  max-width: 900px;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  padding: 1rem 2rem;
+`;
